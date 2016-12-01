db2b5a3b00ea1e469aded28484424df705d08ab2
Jason Jewik <jj11d7t@gmail.com>
2016-11-30 16:17:49 -0800

    RedAutoOp 5

--- TeamCode/src/main/java/org/whs542/ftc2017/RedAutoPlay5.java
+++ TeamCode/src/main/java/org/whs542/ftc2017/RedAutoPlay5.java
@@ -16,10 +16,10 @@ public class RedAutoPlay5 extends OpMode {
     String stateInfo;
     double[] powers = {0.7, 0.8};
     final int startingPosition = 1; //1 or 2
+    long particleDelay = 300;
     //Wheels, Legos, Tools, Gears
     Position[] beaconPositions = {new Position(300,1800,150), new Position(-900,1800,150), new Position(-1800,900,150), new Position(-1800,-300,150)};
-    Position[] redPositions = {new Position(100,100,100), new Position(100,100,100), new Position(0,0,0)};
-    Position[] bluePositions = {new Position(100,100,100), new Position(100,100,100), new Position(0,0,0)};
+    Position[] redPositions = {new Position(-1650,600,100), new Position(-1650,600,150), new Position(0,0,150), new Position(-1800, 1000, 150) };
     Position[] vortexPositions = {new Position(300, 300, 150), new Position(-300, -300, 150)};
 
     @Override
@@ -33,39 +33,37 @@ public class RedAutoPlay5 extends OpMode {
 
         switch(state){
             case 0:
-
-                stateInfo = "turning to vortex";
+                stateInfo = "Turning to vortex";
                 robot.rotateToVortex(vortexPositions[1]);
-
-                if(!robot.rotateToTargetInProgress){
-                    state++;
-                }
+                if (!robot.rotateToTargetInProgress) state++;
                 break;
             case 1:
                 stateInfo = "Shooting particles";
-                robot.flywheel.setFlywheelPower(powers[startingPosition - 1]);
-                if(robot.flywheel.isFlywheelAtCorrectSpeed(powers[startingPosition - 1])){
-                    robot.flywheel.operateGate(1.0);
+                robot.flywheel.setFlywheelPower(powers[startingPosition - 1]); //need something to check if it's up to speed
+                if (robot.flywheel.isFlywheelAtCorrectSpeed(powers[startingPosition - 1])) {
+                    robot.flywheel.operateGateNoToggle(true);
+                    robot.intake.runIntake(1.0);
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep(particleDelay);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    robot.flywheel.setFlywheelPower(0);
+                    robot.intake.runIntake(0.0);
+                    robot.flywheel.setFlywheelPower(0.0);
+                    robot.flywheel.operateGateNoToggle(false);
                     state++;
                 }
                 break;
             case 2:
-                stateInfo = "moving to farther beacon";
-                robot.driveToTarget(beaconPositions[2]);
-                if(!robot.driveToTargetInProgress){
+                robot.driveToTarget(redPositions[2]);
+                if (!robot.driveToTargetInProgress) {
+                    stateInfo = "AutoOp Complete. >~<";
                     state++;
                 }
+                stateInfo = "Driving to center vortex";
                 break;
-            case 3:
-                stateInfo = "";
-
+            default: break;
         }
-
+        telemetry.addData("State Info: ", stateInfo);
     }
 }
