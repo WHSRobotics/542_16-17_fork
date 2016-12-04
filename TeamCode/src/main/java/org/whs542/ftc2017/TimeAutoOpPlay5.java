2cb9ff9b0cbbdc480f510cebbf354c362812dde5
LucyWangs <happiness.lucyw@gmail.com>
2016-12-04 06:41:25 -0800

    Working Auto based on time!
    - shoots 1 particle and hits capball, half on platform

--- TeamCode/src/main/java/org/whs542/ftc2017/TimeAutoOpPlay5.java
+++ TeamCode/src/main/java/org/whs542/ftc2017/TimeAutoOpPlay5.java
@@ -16,7 +16,7 @@ public class TimeAutoOpPlay5 extends OpMode{
 
     WHSRobot robot;
     int state;
-    final static double FLYWHEEL_POWER = 0.1;
+    final static double FLYWHEEL_POWER = 0.18;
 
     @Override
     public void init() {
@@ -33,12 +33,13 @@ public class TimeAutoOpPlay5 extends OpMode{
 
         switch (state){
             case 0:
+                robot.flywheel.operateGateNoToggle(false);
                 robot.flywheel.setFlywheelPower(FLYWHEEL_POWER);
                 state++;
                 break;
             case 1:
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(6000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -71,10 +72,61 @@ public class TimeAutoOpPlay5 extends OpMode{
                 state++;
                 break;
             case 7:
-                robot.drivetrain.moveDistanceMilli2(1300);
+                robot.drivetrain.moveDistanceMilli2(-1300);
                 break;
             default: break;
         }
+
+        /*
+        switch (state) {
+            case 0:
+                robot.flywheel.operateGateNoToggle(false);
+                robot.flywheel.setFlywheelPower(FLYWHEEL_POWER);
+                state++;
+                break;
+            case 1:
+                try {
+                    Thread.sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                state++;
+                break;
+            case 2:
+                robot.flywheel.operateGateNoToggle(true);
+                state++;
+                break;
+            case 3:
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                state++;
+                break;
+            case 4:
+                robot.flywheel.setFlywheelPower(0.0);
+                state++;
+                break;
+            case 5:
+                robot.drivetrain.setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                state++;
+                break;
+            case 6:
+                robot.drivetrain.moveDistanceMilli2(-1300);
+                if (time > 20) {
+                    state++;
+                }
+                break;
+
+            case 7:
+                robot.drivetrain.setLRPower(0, 0);
+                break;
+
+            default:
+                break;
+        }
+        */
         telemetry.addData("State", state);
         telemetry.addData("EncTicks", robot.drivetrain.getEncoderPosition());
 
