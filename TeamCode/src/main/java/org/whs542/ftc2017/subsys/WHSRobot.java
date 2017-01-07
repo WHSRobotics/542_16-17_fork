576040f06e6b79201f63ffec471b44c90244f440
JiangdaZhao <jdz3927@gmail.com>
2017-01-07 14:52:23 -0800

    Power levels

--- TeamCode/src/main/java/org/whs542/ftc2017/subsys/WHSRobot.java
+++ TeamCode/src/main/java/org/whs542/ftc2017/subsys/WHSRobot.java
@@ -39,7 +39,7 @@ public class WHSRobot
     private static final double[] DRIVE_TO_TARGET_POWER_LEVEL = {0.28, 0.5, 0.6, 1.0};
     private static final double DEADBAND_DRIVE_TO_TARGET = 110; //in mm
     private static final double[] DRIVE_TO_TARGET_THRESHOLD = {DEADBAND_DRIVE_TO_TARGET, 300, 600, 1200};
-    private static final double[] ROTATE_TO_TARGET_POWER_LEVEL = {0.25, 0.7, 0.8};
+    private static final double[] ROTATE_TO_TARGET_POWER_LEVEL = {0.35, 0.7, 0.8};
     private static final double DEADBAND_ROTATE_TO_TARGET = 3.5; //in degrees
     private static final double[] ROTATE_TO_TARGET_THRESHOLD = {DEADBAND_ROTATE_TO_TARGET, 45, 90};
 
