4478963c17482f41ea047547fc0afc607eaa4dc5
LucyWangs <happiness.lucyw@gmail.com>
2017-02-22 17:01:45 -0800

    Auto Position changes

--- TeamCode/src/main/java/org/whs542/ftc2017/autoops/WHSAuto.java
+++ TeamCode/src/main/java/org/whs542/ftc2017/autoops/WHSAuto.java
@@ -16,7 +16,7 @@ import org.whs542.lib.Timer;
  * State Machine Based Auto
  */
 
-@Autonomous(name = "WHSAUTO", group = "auto")
+@Autonomous(name = "WHSAuto", group = "auto")
 //@Disabled
 public class WHSAuto extends OpMode {
 
@@ -42,6 +42,7 @@ public class WHSAuto extends OpMode {
     static final int BEACON_2 = 1;
     static final double BEACON_BUTTON_SEPARATION = 140;
     static double[] beaconCaptureHeading = new double[2];
+    static Position[] pullOffWallPositions = new Position[2];
 
     //currentState Definitions
     static final int WARMUP_FLYWHEEL = 0;
@@ -61,10 +62,10 @@ public class WHSAuto extends OpMode {
         stateEnabled[WARMUP_FLYWHEEL] = true;
         stateEnabled[SHOOT_PARTICLE_1] = true;
         stateEnabled[SHOOT_PARTICLE_2] = true;
-        stateEnabled[CAPTURE_BEACON_1] = false;
-        stateEnabled[CAPTURE_BEACON_2] = false;
+        stateEnabled[CAPTURE_BEACON_1] = true;
+        stateEnabled[CAPTURE_BEACON_2] = true;
         stateEnabled[DRIVE_TO_BEACON_WALL] = stateEnabled[CAPTURE_BEACON_1] | stateEnabled[CAPTURE_BEACON_2]; //Should technically be above CAPTURE_BEACON_1
-        stateEnabled[KNOCK_CAP_BALL] = true;
+        stateEnabled[KNOCK_CAP_BALL] = false;
         stateEnabled[PARK_ON_CENTER] = true;
         stateEnabled[EXIT] = true;
 
@@ -88,14 +89,17 @@ public class WHSAuto extends OpMode {
 
     //Beacon Control
     static final double BEACON_RETRACTION_DELAY = 1.2; //In seconds
-    static final double BEACON_DRIVE_POWER = 0.30; //Arbitrary value
+    static final double BEACON_DRIVE_POWER = 0.70; //Arbitrary value
 
     //Flywheel Control
-    double[] powers = {0.67, 0.8};
+    double[] powers = {0.69, 0.8};
     final int startingPosition = 1; //1 or 2
     static final double FLYWHEEL_WARMUP_DELAY = 6.0; //in seconds
     static final double PARTICLE_UP_PUSHER_DELAY = 1.5;
     static final double PARTICLE_DOWN_PUSHER_DELAY = 1.5;
+
+    //Cap Ball Positions
+    static Position[] capBallPositions = new Position[2];
 
     //Vuforia Target Locations
     //Wheels, Legos, Tools, Gears
@@ -149,7 +153,11 @@ public class WHSAuto extends OpMode {
         //Directions robot is facing to parallel with beacons wall (in degrees)
         beaconCaptureHeading[RED] = 0;
         beaconCaptureHeading[BLUE] = Functions.normalizeAngle(180);
+        pullOffWallPositions[RED] = new Position(-300,-900,150);
+        pullOffWallPositions[BLUE] = new Position(900,900,150);
 
+        capBallPositions[BLUE] = new Position(680,1490,150);
+        capBallPositions[RED] = new Position(520,1490,150);
     }
 
     @Override
@@ -277,19 +285,22 @@ public class WHSAuto extends OpMode {
 
                 if(!pullOffWallComplete)
                 {
-                    robot.driveToTarget(new Position(-300, -900, 150));
+                    currentStateName = "pulling off wall";
+                    robot.driveToTarget(pullOffWallPositions[ALLIANCE]/*new Position(-300, -900, 150)*/);
                     if(!robot.driveToTargetInProgress & !robot.rotateToTargetInProgress) {
                         pullOffWallComplete = true;
                     }
                 }
                 else if(!driveToBeaconWallComplete)
                 {
+                    currentStateName = "driving to beacon wall";
                     robot.driveToTarget(beaconPostionArray[ALLIANCE][0]);
                     if(!robot.driveToTargetInProgress & !robot.rotateToTargetInProgress){
                         driveToBeaconWallComplete = true;
                     }
                 }
                 else if(!rotateToParallelWallComplete){
+                    currentStateName = "rotating to parallele wall";
                     robot.rotateToTarget(beaconCaptureHeading[ALLIANCE]);
                     if(!robot.driveToTargetInProgress & !robot.rotateToTargetInProgress){
                         rotateToParallelWallComplete = true;
@@ -401,7 +412,7 @@ public class WHSAuto extends OpMode {
                 //state processing
                 if(!pullOffWallComplete)
                 {
-                    robot.driveToTarget(new Position(-300, -900, 150));
+                    robot.driveToTarget(pullOffWallPositions[ALLIANCE]);
                     if(!robot.driveToTargetInProgress & !robot.rotateToTargetInProgress) {
                         pullOffWallComplete = true;
                     }
